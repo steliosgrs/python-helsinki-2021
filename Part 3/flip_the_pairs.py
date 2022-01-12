a73d6c0995725d73635768d4e0454e842dddb7be
NoLiferX <steliosgp13@gmail.com>
2022-01-12 19:14:12 +0200

    End of part 3

--- Part 3/flip_the_pairs.py
+++ Part 3/flip_the_pairs.py
@@ -1,1 +1,20 @@
 # Write your solution here
+
+num = int(input("Please type in a number: "))
+numbers=[]
+for i in range(1,num+1):
+    numbers.append(i)
+
+# print(numbers)
+j=0
+
+while j < len(numbers)-1:
+    if j%2==0 or j==0:
+        temp=numbers[j]
+        numbers[j]=numbers[j+1]
+        numbers[j+1]=temp
+    j+=1
+
+for i in numbers:
+    print(i)  
+
--- Part 4/shape.py
+++ Part 4/shape.py
@@ -1,4 +1,18 @@
 # Copy here code of line function from previous exercise and use it in your solution
+
+def line(x,y):
+    if y=="":
+        print(x*"*")
+    else:
+        print(x*y[0])
+
+def shape(x,text1,y,text2):
+    for i in range(x+1):
+        line(i,text1)
+    for i in range(y):
+        line(x,text2)
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    shape(5, "x", 2, "o")
+    shape(5, "X", 3, "*")
+
